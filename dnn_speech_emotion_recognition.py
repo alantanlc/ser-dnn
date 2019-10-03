6afb56f5d1dbfa6de25695b3bbd02470d485f426
Alan Tan <alanwuha91@gmail.com>
2019-10-03 15:49:49 +0800

    updated main script

--- dnn_speech_emotion_recognition.py
+++ dnn_speech_emotion_recognition.py
@@ -71,3 +71,55 @@ n_hidden_units_1 = n_dim
 n_hidden_units_2 = 400 # approx n_dim * 2
 n_hidden_units_3 = 200 # half of layer 2
 n_hidden_units_4 = 100
+
+def create_model(activation_function='relu', init_type='normal', optimiser='adam', dropout_rate=0.2):
+    model = Sequential()
+    # layer 1
+    model.add(Dense(n_hidden_units_1, input_dim=n_dim, init=init_type, activation=activation_function))
+    # layer 2
+    model.add(Dense(n_hidden_units_2, init=init_type, activation=activation_function))
+    model.add(Dropout(dropout_rate))
+    # layer 3
+    model.add(Dense(n_hidden_units_3, init=init_type, activation=activation_function))
+    model.add(Dropout(dropout_rate))
+    # layer 4
+    model.add(Dense(n_hidden_units_4, init=init_type, activation=activation_function))
+    model.add(Dropout(dropout_rate))
+    # output later
+    model.add(Dense(n_classes, init=init_type, activation='softmax'))
+    # model compilation
+    model.compile(loss='categorical_crossentropy', optimizer=optimiser, metrics=['accuracy'])
+    return model
+
+# create the mode
+model = create_model()
+
+# train the model
+history = model.fit(train_x, train_y, epochs=200, batch_size=4)
+
+# predicting from the model
+predict = model.predict(test_x, batch_size=4)
+
+# predicted emotions from the test set
+emotions = ['neutral', 'calm', 'happy', 'sad', 'angry', 'fearful', 'disgust', 'surprised']
+y_pred = np.argmax(predict, 1)
+predicted_emo = []
+for i in range(0, test_y.shape[0]):
+    emo = emotions[y_pred[i]]
+    predicted_emo.append(emo)
+
+actual_emo = []
+y_true = np.argmax(test_y, 1)
+for i in range(0, test_y.shape[0]):
+    emo = emotions[y_true[i]]
+    actual_emo.append(emo)
+
+# generate the confusion matrix
+cm = confusion_matrix(actual_emo, predicted_emo)
+index = ['angry', 'calm', 'disgust', 'fearful', 'happy', 'neutral', 'sad', 'surprise']
+columns = ['angry', 'calm', 'disgust', 'fearful', 'happy', 'neutral', 'sad', 'surprise']
+cm_df = pd.DataFrame(cm, index, columns)
+plt.figure(figsize=(10,6))
+sns.heatmap(cm_df, annot=True)
+
+print('End of program')
